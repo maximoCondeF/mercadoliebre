b00e424c3c2d3c9279bbf7b514414a2aab022c5b
Maximo <maxiconde02@gmail.com>
2023-08-23 15:40:09 -0300

    agregados todos los cambios de mercadoliebre modulo 4

--- app.js
+++ app.js
@@ -12,4 +12,16 @@ app.listen(PORT,()=>console.log("Up and running, mate"));
 
 app.get("/", (req,res)=>{
     res.sendFile((path.join(__dirname, "./views/home.html")))
-})
+});
+
+app.get("/product-creation", (req,res)=>{
+    res.sendFile((path.join(__dirname, "./views/product-creation.html")))
+});
+
+app.get("/register", (req,res)=>{
+    res.sendFile((path.join(__dirname, "./views/register.html")))
+});
+
+app.get("/login", (req,res)=>{
+    res.sendFile((path.join(__dirname, "./views/login.html")))
+});
