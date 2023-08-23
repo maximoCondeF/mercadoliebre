7c3cf7c122beb133978fe620dc501114e1b17363
Maximo <maxiconde02@gmail.com>
2023-08-23 15:50:19 -0300

    Agregado puerto dinamico a app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const PORT = 3030
+const PORT = process.env.PORT || 3030;
 
 const publicPath = path.resolve(__dirname+"/public");
 console.log(publicPath);
